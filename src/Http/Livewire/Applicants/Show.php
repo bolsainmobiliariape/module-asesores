12599386fd0c6c0ccae743d807b00ca3ce3d5ff5
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2022-02-11 11:30:49 -0600

    add show

---
+++ src/Http/Livewire/Applicants/Show.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bolsainmobiliariape\ModuleAsesores\Http\Livewire\Applicants;
+
+use Bolsainmobiliariape\ModuleProjects\Models\Applicant;
+use Livewire\Component;
+
+class Show extends Component
+{
+
+    public Applicant $applicant;
+
+    public $names;
+
+
+    public function markAsContacted($id)
+    {
+        $applicant = Applicant::find($id);
+
+        $applicant->contacted = !$applicant->contacted;
+
+        $applicant->save();
+    }
+
+    public  function mount()
+    {
+        $this->names = config('module-asesores.applicants.names');
+    }
+
+    public function render()
+    {
+        return view('module-asesores::dashboard.applicants.show')
+            ->layoutData(['header' => 'Aplicantes / Detalle']);
+    }
+}
